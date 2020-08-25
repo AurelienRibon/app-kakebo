3953b3233c0388620d5059a90e7cbfac5bad492e
Aurelien Ribon <aurelien.ribon@gmail.com>
2020-08-25 10:45:52 +0200

    Move v-ripple.js to directives/

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
 import App from './App.vue';
-import './lib/v-ripple.js';
+import './directives/v-ripple.js';
 
 new Vue({ render: (createElement) => createElement(App) }).$mount('#app');
