62933b1f1276ca0771cdfc2ebbd0f62fc89db438
Aurelien Ribon <aurelien.ribon@gmail.com>
2020-10-31 16:56:56 +0100

    Add `Expense.type`, as well as related types

--- dev-client/src/models/expense.ts
+++ dev-client/src/models/expense.ts
@@ -1,14 +1,20 @@
 import { getUnknownCategoryName, hasCategoryName } from '../lib/categories';
 
-export default class Expense {
+export const EXPENSE_TYPES = ['card', 'cash', 'check', 'transfer'] as const;
+
+export type ExpenseType = typeof EXPENSE_TYPES[number];
+
+export class Expense {
   date: Date;
   category: string;
   amount: number;
+  type: ExpenseType;
 
   constructor(spec: Record<string, unknown> = {}) {
     this.date = this.setupDate(spec.date);
     this.category = this.setupCategory(spec.category);
     this.amount = this.setupAmount(spec.amount);
+    this.type = this.setupType(spec.type);
   }
 
   private setupDate(date: unknown): Date {
@@ -23,5 +29,11 @@ export default class Expense {
 
   private setupAmount(amount: unknown): number {
     return typeof amount === 'number' ? amount : 0;
+  }
+
+  private setupType(type: unknown): ExpenseType {
+    return (EXPENSE_TYPES as readonly unknown[]).includes(type)
+      ? (type as ExpenseType)
+      : 'card';
   }
 }
