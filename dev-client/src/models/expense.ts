f60f7981c25e61c994ba5f66e3ca6c457bc36ea8
Aurelien Ribon <aurelien.ribon@gmail.com>
2021-01-01 18:23:38 +0100

    Add label and comment to Expense

--- dev-client/src/models/expense.ts
+++ dev-client/src/models/expense.ts
@@ -2,19 +2,25 @@ import { formatDateToDay } from '../lib/dates';
 import { getExpenseDefaultType } from '../lib/expenses';
 import { guid } from '../lib/utils';
 
+const DEFAULT_TYPE = getExpenseDefaultType();
+
 export class Expense {
   public readonly id: string;
   public readonly date: Date;
   public readonly amount: number;
   public readonly category: string;
   public readonly type: string;
+  public readonly label: string;
+  public readonly comment: string;
 
-  constructor(date: Date, amount: number, category?: string, type?: string) {
+  constructor(date = new Date(), amount = 0, category = 'unknown', type = DEFAULT_TYPE, label = '', comment = '') {
     this.id = guid();
     this.date = date;
     this.amount = amount;
-    this.category = category || 'unknown';
-    this.type = type || getExpenseDefaultType();
+    this.category = category;
+    this.type = type;
+    this.label = label;
+    this.comment = comment;
   }
 
   serialize(): Record<string, unknown> {
@@ -23,6 +29,8 @@ export class Expense {
       amount: this.amount,
       category: this.category,
       type: this.type,
+      label: this.label,
+      comment: this.comment,
     };
   }
 }
