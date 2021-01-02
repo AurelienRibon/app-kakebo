1502321c9f5d7afa8c292a75336d046ea4a1f11d
Aurelien Ribon <aurelien.ribon@gmail.com>
2021-01-02 09:34:10 +0100

    Pass chosen category to expense details panel

--- dev-client/src/components/AddExpense.vue
+++ dev-client/src/components/AddExpense.vue
@@ -5,7 +5,12 @@
 <template>
   <div>
     <AddExpenseCategory v-if="index === 1" @done="onCategoryDone"></AddExpenseCategory>
-    <AddExpenseDetails v-if="index === 2" @done="onDetailsDone" @cancel="onDetailsCancel"></AddExpenseDetails>
+    <AddExpenseDetails
+      v-if="index === 2"
+      :category="category"
+      @done="onDetailsDone"
+      @cancel="onDetailsCancel"
+    ></AddExpenseDetails>
   </div>
 </template>
 
@@ -24,22 +29,22 @@
 
     setup(props, { emit }) {
       const index = ref(1);
-      let category = '';
+      const category = ref('');
+
+      return { index, category, onCategoryDone, onDetailsDone, onDetailsCancel };
 
       function onCategoryDone(cat: string): void {
-        category = cat;
+        category.value = cat;
         index.value += 1;
       }
 
       function onDetailsDone(details: Record<string, unknown>): void {
-        emit('done', { category, ...details });
+        emit('done', { category: category.value, ...details });
       }
 
       function onDetailsCancel(): void {
         emit('cancel');
       }
-
-      return { index, onCategoryDone, onDetailsDone, onDetailsCancel };
     },
   });
 </script>
