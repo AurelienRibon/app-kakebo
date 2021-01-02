af075bc31b8ea9d302cfc954c4e03e065e7e9727
Aurelien Ribon <aurelien.ribon@gmail.com>
2021-01-02 08:25:52 +0100

    Add function `extractExpensesLabels`

--- dev-client/src/lib/expenses.ts
+++ dev-client/src/lib/expenses.ts
@@ -57,3 +57,18 @@ export function splitExpensesByDay(expenses: Expense[]): SameDayExpenses[] {
 
   return result;
 }
+
+export function extractExpensesLabels(expenses: Expense[], category: string): string[] {
+  const counts = new Map() as Map<string, number>;
+
+  for (const expense of expenses) {
+    if (expense.label && expense.category === category) {
+      const count = counts.get(expense.label) || 0;
+      counts.set(expense.label, count + 1);
+    }
+  }
+
+  return Array.from(counts.entries())
+    .sort((a, b) => a[1] - b[1])
+    .map((it) => it[0]);
+}
