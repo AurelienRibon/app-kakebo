e2ca03d1cb8b19b47945708f0e177fddeb0a04b1
Aurelien Ribon <aurelien.ribon@gmail.com>
2021-01-03 08:46:52 +0100

    Improve v-tap detection of tap

--- dev-client/src/directives/v-tap.ts
+++ dev-client/src/directives/v-tap.ts
@@ -3,13 +3,21 @@ import { Directive } from 'vue';
 export function getVTapDirective(): Directive {
   return {
     mounted(el: HTMLElement) {
-      el.addEventListener('touchstart', () => {
-        const startTime = Date.now();
+      el.addEventListener('touchstart', (event1: TouchEvent) => {
+        const t1 = Date.now();
+        const x1 = event1.changedTouches[0].screenX;
+        const y1 = event1.changedTouches[0].screenY;
 
-        const touchendListener = () => {
+        const touchendListener = (event2: TouchEvent) => {
           el.removeEventListener('touchend', touchendListener);
 
-          if (Date.now() - startTime < 200) {
+          const t2 = Date.now();
+          const x2 = event2.changedTouches[0].screenX;
+          const y2 = event2.changedTouches[0].screenY;
+          const xDiff = Math.abs(x1 - x2);
+          const yDiff = Math.abs(y1 - y2);
+
+          if (t2 - t1 < 200 && xDiff < 5 && yDiff < 5) {
             el.dispatchEvent(new Event('tap'));
           }
         };
