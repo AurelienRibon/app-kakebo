feb350651d5055a237c04aefd90b272a881ef9b5
Aurelien Ribon <aurelien.ribon@gmail.com>
2021-01-14 11:24:48 +0100

    Keep a list of all expenses in store

--- dev-client/src/store/store.ts
+++ dev-client/src/store/store.ts
@@ -5,6 +5,7 @@ import { readFile, writeFile } from '../lib/fs';
 import { str } from '../lib/utils';
 
 class Store {
+  private _expensesFull = ref([]) as Ref<Expense[]>;
   private _expenses = ref([]) as Ref<Expense[]>;
   private _version = 1;
 
@@ -19,15 +20,17 @@ class Store {
   addExpense(spec: ExpenseJSON): void {
     const expense = createExpenseFromJSON(spec);
     this._expenses.value.push(expense);
+    this._expensesFull.value.push(expense);
     sortExpenses(this._expenses.value);
   }
 
   editExpense(expense: Expense, spec: ExpenseJSON): void {
-    const index = this.getExpenseIndex(expense);
-    this._expenses.value.splice(index, 1, createExpenseFromJSON(spec));
+    this.removeExpense(expense);
+    this.addExpense(spec);
   }
 
   removeExpense(expense: Expense) {
+    expense.delete();
     const index = this.getExpenseIndex(expense);
     this._expenses.value.splice(index, 1);
   }
@@ -37,22 +40,24 @@ class Store {
       const content = await readFile('data.json');
       const spec = JSON.parse(content);
       const specExpenses = spec.expenses as ExpenseJSON[];
-      this._expenses.value = createExpensesFromJSON(specExpenses);
+      this._expensesFull.value = createExpensesFromJSON(specExpenses);
+      this._expenses.value = this._expensesFull.value.filter((it) => !it.deleted);
     } catch (err) {
+      this._expensesFull.value = [];
       this._expenses.value = [];
     }
   }
 
   async save(): Promise<void> {
     const content = JSON.stringify({
-      expenses: this._expenses.value.map((it) => it.serialize()),
+      expenses: this._expensesFull.value.map((it) => it.serialize()),
       version: this._version,
     });
     await writeFile('data.json', content);
   }
 
   private getExpenseIndex(expense: Expense): number {
-    const index = this._expenses.value.findIndex((it) => it._id === expense._id);
+    const index = this._expenses.value.findIndex((it) => it.id === expense.id);
     if (index === -1) {
       throw new Error(`Expense not found: ${str(expense)}`);
     }
