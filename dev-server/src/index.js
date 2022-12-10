61e89efd4204e5b07f7ffe388ddd718c401ed028
Aurelien Ribon <aurelien.ribon@gmail.com>
2022-12-10 12:33:38 +0100

    Upgrade dependencies

--- dev-server/src/index.js
+++ dev-server/src/index.js
@@ -22,7 +22,7 @@ setupAndStart();
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', '*');
   next();
 });
 
