d8673a91ae2408824822a18674b3b69c9673fd23
Aurelien Ribon <aurelien.ribon@gmail.com>
2023-09-19 22:11:29 +0200

    Lint

--- dev-client/src/lib/expense-periodicities.ts
+++ dev-client/src/lib/expense-periodicities.ts
@@ -1,6 +1,6 @@
 const PERIODICITIES = ['one-time', 'monthly'] as const;
 
-export type ExpensePeriodicity = typeof PERIODICITIES[number];
+export type ExpensePeriodicity = (typeof PERIODICITIES)[number];
 
 export function isExpensePeriodicityValid(value: unknown): value is ExpensePeriodicity {
   return typeof value === 'string' && (PERIODICITIES as readonly string[]).includes(value);
